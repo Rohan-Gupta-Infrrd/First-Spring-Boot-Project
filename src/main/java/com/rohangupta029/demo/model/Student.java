146756f1eacef3cc0c0d44f912c78b5a42665074
Rohan-Gupta-Infrrd <rohangupta@infrrd.ai>
2022-11-29 11:26:40 +0530

    Student Class Added

--- src/main/java/com/rohangupta029/demo/model/Student.java
+++ src/main/java/com/rohangupta029/demo/model/Student.java
@@ -1,5 +1,67 @@
 package com.rohangupta029.demo.model;
 
+import java.util.UUID;
+
 public class Student {
+	private final UUID id;
+	private final int age;
+	private String Firstname;
+	private String Lastname;
+	private String course;
+	
+	
+	public Student(UUID id, int age, String firstname, String lastname, String course) {
+		super();
+		this.id = id;
+		this.age = age;
+		Firstname = firstname;
+		Lastname = lastname;
+		this.course = course;
+	}
+
+
+	public String getFirstname() {
+		return Firstname;
+	}
+
+
+	public void setFirstname(String firstname) {
+		Firstname = firstname;
+	}
+
+
+	public String getLastname() {
+		return Lastname;
+	}
+
+
+	public void setLastname(String lastname) {
+		Lastname = lastname;
+	}
+
+
+	public String getCourse() {
+		return course;
+	}
+
+
+	public void setCourse(String course) {
+		this.course = course;
+	}
+
+
+	public UUID getId() {
+		return id;
+	}
+
+
+	public int getAge() {
+		return age;
+	}
+	
+	
+	
+	
+	
 
 }
